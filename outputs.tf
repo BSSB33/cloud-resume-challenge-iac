27db7176bee5b9eae124483a65050676c5c2328d
Vitrai Gábor <vitraig@gmail.com>
2025-11-30 19:00:16 +0100

    Source Files Uploaded to S3 Bucket

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,15 @@ output "s3_bucket_region" {
   description = "Region of the S3 bucket"
   value       = aws_s3_bucket.cloud_resume.region
 }
+
+output "uploaded_files" {
+  description = "List of uploaded website files"
+  value       = keys(aws_s3_object.website_files)
+}
+
+output "website_files_etags" {
+  description = "ETags of uploaded files (for cache invalidation)"
+  value = {
+    for key, obj in aws_s3_object.website_files : key => obj.etag
+  }
+}
